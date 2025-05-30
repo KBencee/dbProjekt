0a355c972d1cf6e38900bef716d06866c232e8a2
Kiss Bence <kiss.bence@mechwart.local>
2025-05-30 08:49:51 +0200

    kesz

--- Sportbolt.sql
+++ Sportbolt.sql
@@ -1,3 +1,9 @@
+DROP DATABASE IF EXISTS sportbolt;
+
+CREATE DATABASE sportbolt DEFAULT CHARACTER SET utf8 COLLATE utf8_hungarian_ci;
+
+USE sportbolt;
+
 CREATE TABLE `Termek` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `nev` varchar(255),
@@ -32,6 +38,19 @@ CREATE TABLE `Rendeles_Tartalmaz` (
   `darabszam` int
 );
 
+CREATE TABLE `Szallito` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `nev` varchar(255),
+  `telefonszam` varchar(255),
+  `orszag` varchar(255)
+);
+
+CREATE TABLE `Szallitja` (
+  `rendeles_id` int,
+  `szallito_id` int,
+  `szallitas_ido` datetime
+);
+
 ALTER TABLE `Termek` ADD FOREIGN KEY (`gyarto_id`) REFERENCES `Gyarto` (`id`);
 
 ALTER TABLE `Rendeles` ADD FOREIGN KEY (`vasarlo_id`) REFERENCES `Vasarlo` (`id`);
@@ -39,3 +58,11 @@ ALTER TABLE `Rendeles` ADD FOREIGN KEY (`vasarlo_id`) REFERENCES `Vasarlo` (`id`
 ALTER TABLE `Rendeles_Tartalmaz` ADD FOREIGN KEY (`rendeles_id`) REFERENCES `Rendeles` (`id`);
 
 ALTER TABLE `Rendeles_Tartalmaz` ADD FOREIGN KEY (`termek_id`) REFERENCES `Termek` (`id`);
+
+ALTER TABLE `Szallitja` ADD FOREIGN KEY (`rendeles_id`) REFERENCES `Rendeles` (`id`);
+
+ALTER TABLE `Szallitja` ADD FOREIGN KEY (`szallito_id`) REFERENCES `Szallito` (`id`);
+
+ALTER TABLE `Rendeles_Tartalmaz` ADD PRIMARY KEY (`rendeles_id`, `termek_id`);
+
+ALTER TABLE `Szallitja` ADD PRIMARY KEY (`rendeles_id`, `szallito_id`);
